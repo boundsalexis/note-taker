f8d88c2fbf563a3109c5dd3031cf06b970dcbb67
Alexis <lxs.bnds@gmail.com>
2020-01-12 14:58:38 -0800

    server.js created, routes not set up yet

---
+++ server.js
@@ -0,0 +1,25 @@
+///////////////// DEPENDENCIES////////////
+var express = require("express");
+
+//////////////// CONFIGURE EXPRESS/////////
+
+/// create server
+var app = express();
+
+//set initial port
+var PORT = process.env.PORT || 3000;
+
+// sets our server to handle parsing data
+app.use(express.urlencoded({ extened: true }));
+app.use(express.json());
+
+////////////////ROUTING///////////////////
+
+require("./public/routes/apiRoutes")(app);
+require("./public/routes/htmlRoutes")(app);
+
+///////////////LISTENER aka LAUNCHER///////////
+
+app.listen(PORT, function(){
+    console.log("App listening on Port: " + PORT);
+});
