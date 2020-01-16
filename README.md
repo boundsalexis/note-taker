ed6548eca8b678a8ca883610c7fa5b39c66ed70d
Alexis <lxs.bnds@gmail.com>
2020-01-15 20:58:14 -0800

    readme complete

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # note-taker
-persistent note taking app for a persistent person
+Take notes, and never lose them
+
+# links
+> [GitHub Repository](https://github.com/boundsalexis/note-taker)
+
+## Images
+![Final Product](3m9mjs.gif)
+
+# Technologies Used
+> Node JS 
+> FS to write files
+> Path for working with files
+> CSS to style the output html
+
+
+# Summary
+AS A user, I want to be able to write and save notes
+I WANT to be able to delete notes I've written before
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+
+# Code Snippet
+``` javascript
+ app.post("/api/notes", function (req, res) {
+        // reads our database file
+        fs.readFile(path.join(__dirname, "../../db/db.json"), "utf-8", function (err, data) {
+            // turns into object
+            let database = JSON.parse(data);
+
+            // pushed new note into database
+            database.push(req.body);
+            // adds unique id every time new note is added
+            for (let i = 0; i < database.length; i++) {
+                database[i].id = i;
+            }
+            // rewrites our database file with the new info
+            fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(database), function (err) {
+                if (err) throw err;
+            });
+
+        });
+    })
+```
+> This is how we ensure each note always has a unqie id
+> New note gets added
+> Every element in database array gets id attribute rewritten or added in the case of the new note
+> This is how we ensure when we delete a note we are deleting the right one
+
+# Upgrades
+> Adding folders to organize the notes
+
+
+## Authors
+
+* **Alexis Bounds** 
+
+- [Link to Portfolio Site](https://github.com/boundsalexis/basic-portfolio)
+- [Link to Github](https://github.com/boundsalexis)
+- [Link to LinkedIn](https://www.linkedin.com/in/alexis-bounds-9b7711169/)
--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -10,32 +10,38 @@ module.exports = function (app) {
 
 
     app.get("/api/notes", function (req, res) {
+        //sends database file to display
         res.sendFile(path.join(__dirname, "../../db/db.json"))
     });
     app.post("/api/notes", function (req, res) {
-
+        // reads our database file
         fs.readFile(path.join(__dirname, "../../db/db.json"), "utf-8", function (err, data) {
+            // turns into object
             let database = JSON.parse(data);
-            console.log(database);
-            database.push(req.body);
 
+            // pushed new note into database
+            database.push(req.body);
+            // adds unique id every time new note is added
             for (let i = 0; i < database.length; i++) {
                 database[i].id = i;
             }
-            let stuff = JSON.stringify(database);
-            fs.writeFile(path.join(__dirname, "../../db/db.json"), stuff, function (err) {
+            // rewrites our database file with the new info
+            fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(database), function (err) {
                 if (err) throw err;
             });
 
         });
     })
     app.delete("/api/notes/:id", function (req, res) {
-        console.log("delete clicked");
+        //reads our database file
         fs.readFile(path.join(__dirname, "../../db/db.json"), "utf-8", function (err, data) {
+            //turns into object
             let database = JSON.parse(data);
+            //grabs id from url
             let id = parseInt(req.params.id);
+            // filters notes that do not match that id into new  variable
             let filteredDatabase = database.filter(note => note.id != id);
-
+            // writes new data back to database
             fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(filteredDatabase), function (err) {
                 if (err) throw err;
             })
